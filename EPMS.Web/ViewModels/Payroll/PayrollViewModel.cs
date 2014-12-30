5639b76b861e6ce5a19cf35e24640e89228db230
istammar <ammar@innostark.com>
2014-12-30 14:26:07 +0500

    Payroll

---
+++ EPMS.Web/ViewModels/Payroll/PayrollViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPMS.Web.ViewModels.Payroll
+{
+    public class PayrollViewModel
+    {
+    }
+}
