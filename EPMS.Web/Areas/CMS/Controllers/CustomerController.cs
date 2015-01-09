a551fb97b25097be7d609d9a64965a5a2cdef2f7
hassanist <hassan@innostark.com>
2015-01-09 18:10:05 +0500

    Customer
    Controller
    Mapper
    Resource
    View

---
+++ EPMS.Web/Areas/CMS/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using EPMS.Interfaces.IServices;
+using EPMS.Models.DomainModels;
+using EPMS.Models.IdentityModels.ViewModels;
+using EPMS.Web.Controllers;
+using EPMS.Web.ModelMappers;
+using EPMS.Web.ViewModels.Customer;
+using Microsoft.AspNet.Identity;
+
+namespace EPMS.Web.Areas.CMS.Controllers
+{
+    public class CustomerController : BaseController
+    {
+        #region Private
+
+        private readonly ICustomerService customerService;
+
+        #endregion
+
+        #region Constructor
+
+        public CustomerController(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+        }
+
+        #endregion
+
+        #region Public
+
+        public ActionResult Index()
+        {
+            return View(new CustomerViewModel
+            {
+                CustomerList = customerService.GetAll().Select(x => x.CreateFrom())
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ EPMS.Web/ModelMappers/CustomerMapper.cs
@@ -0,0 +1,40 @@
+﻿using ApiModels = EPMS.Web.Models;
+using DomainModels = EPMS.Models.DomainModels;
+
+namespace EPMS.Web.ModelMappers
+{
+    public static class CustomerMapper
+    {
+        public static Models.Customer CreateFrom(this DomainModels.Customer source)
+        {
+            return new Models.Customer
+            {
+                CustomerId = source.CustomerId,
+                CustomerName = source.CustomerName,
+                CustomerAddress = source.CustomerAddress,
+                CustomerMobile = source.CustomerMobile,
+                RecCreatedBy = source.RecCreatedBy,
+                RecCreatedDt = source.RecCreatedDt,
+                RecLastUpdatedBy = source.RecLastUpdatedBy,
+                RecLastUpdatedDt = source.RecLastUpdatedDt,
+            };
+
+        }
+
+        public static DomainModels.Customer CreateFrom(this Models.Customer source)
+        {
+            return new DomainModels.Customer
+            {
+                CustomerId = source.CustomerId,
+                CustomerName = source.CustomerName,
+                CustomerAddress = source.CustomerAddress,
+                CustomerMobile = source.CustomerMobile,
+                RecCreatedBy = source.RecCreatedBy,
+                RecCreatedDt = source.RecCreatedDt,
+                RecLastUpdatedBy = source.RecLastUpdatedBy,
+                RecLastUpdatedDt = source.RecLastUpdatedDt
+            };
+
+        }
+    }
+}
