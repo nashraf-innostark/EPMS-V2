e41ff10978397756f67e8ff30c317bb9f0c1be55
hassanist <hassan@innostark.com>
2015-01-09 17:26:19 +0500

    Customer Services and Repositories

---
+++ EPMS.Implementation/Services/CustomerService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using EPMS.Interfaces.IServices;
+using EPMS.Interfaces.Repository;
+using EPMS.Models.DomainModels;
+
+namespace EPMS.Implementation.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        #region Constructor
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        #endregion
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return customerRepository.GetAll();
+        }
+
+        public Customer FindCustomerById(long id)
+        {
+            return customerRepository.Find(id);
+        }
+
+        public bool AddCustomer(Customer customer)
+        {
+            customerRepository.Add(customer);
+            customerRepository.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            customerRepository.Update(customer);
+            customerRepository.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ EPMS.Repository/Repositories/CustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using EPMS.Interfaces.Repository;
+using EPMS.Models.DomainModels;
+using EPMS.Repository.BaseRepository;
+using Microsoft.Practices.Unity;
+
+namespace EPMS.Repository.Repositories
+{
+    public sealed class CustomerRepository : BaseRepository<Customer>, ICustomerRepository
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CustomerRepository(IUnityContainer container)
+            : base(container)
+        {
+        }
+        /// <summary>
+        /// Primary database set
+        /// </summary>
+        protected override IDbSet<Customer> DbSet
+        {
+            get { return db.Customers; }
+        }
+    }
+}
