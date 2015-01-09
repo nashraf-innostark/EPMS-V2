2ca6524f5690a77be3ee17539dc21d25655150dc
curiousnadir <bsef09m031@pucit.edu.pk>
2015-01-09 16:14:58 +0500

    Generated Domain Models

---
+++ EPMS.Models/DomainModels/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace EPMS.Models.DomainModels
+{
+    public class Customer
+    {
+        public long CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerMobile { get; set; }
+
+        public virtual ICollection<AspNetUser> AspNetUsers { get; set; }
+        public virtual ICollection<Complaint> Complaints { get; set; }
+    }
+}
