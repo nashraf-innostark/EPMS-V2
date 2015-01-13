c3b05bf6265d8c60bbe01024c622ba9422ee1d1d
istammar <ammar@innostark.com>
2015-01-13 17:54:19 +0500

    JQ Validation and Quotation

---
+++ EPMS.Web/Areas/CMS/Controllers/QuotationController.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using EPMS.Implementation.Services;
+using EPMS.Interfaces.IServices;
+using EPMS.Web.ModelMappers;
+using EPMS.Web.ViewModels.Quotation;
+using Microsoft.AspNet.Identity;
+
+namespace EPMS.Web.Areas.CMS.Controllers
+{
+    public class QuotationController : Controller
+    {
+        #region Private
+        private readonly ICustomerService CustomerService;
+        private readonly IAspNetUserService AspNetUserService;
+        #endregion
+
+        #region Constructor
+
+        public QuotationController(ICustomerService customerService, IAspNetUserService aspNetUserService)
+        {
+            CustomerService = customerService;
+            AspNetUserService = aspNetUserService;
+        }
+
+        #endregion
+
+        #region Public
+        // GET: CMS/Quotation
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            QuotationCreateViewModel viewModel = new QuotationCreateViewModel();
+            var customers = CustomerService.GetAll();
+            viewModel.Customers = customers.Select(x => x.CreateFromServerToClient());
+            var users = AspNetUserService.FindById(User.Identity.GetUserId());
+            if(users.Employee != null)
+                viewModel.EmployeeName = users.Employee.EmployeeNameE;
+            return View(viewModel);
+        }
+        #endregion
+    }
+}
---
+++ EPMS.Web/ViewModels/Quotation/QuotationCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using EPMS.Models.RequestModels;
+
+namespace EPMS.Web.ViewModels.Quotation
+{
+    public class QuotationCreateViewModel
+    {
+        public QuotationCreateViewModel()
+        {
+            SearchRequest = new QuotationSearchRequest();
+        }
+
+        public IEnumerable<Models.Customer> Customers { get; set; }
+        public string Customer { get; set; }
+        public string EmployeeName { get; set; }
+        public QuotationSearchRequest SearchRequest { get; set; }
+    }
+}
