d55a84f01b6b95bf012be80d77b4ae1975eea6a3
istammar <ammar@innostark.com>
2015-01-19 21:34:06 +0500

    Task List View Start

---
+++ EPMS.Web/Areas/PMS/Controllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPMS.Web.ViewModels.Tasks;
+
+namespace EPMS.Web.Areas.PMS.Controllers
+{
+    public class TaskController : Controller
+    {
+        // GET: PMS/Task
+        public ActionResult Index()
+        {
+            TaskListViewModel viewModel = new TaskListViewModel();
+            return View(viewModel);
+        }
+    }
+}
