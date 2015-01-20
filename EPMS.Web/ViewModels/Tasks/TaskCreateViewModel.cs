65237f53fec07bc00377ab7aee48c9a80e12e26b
istammar <ammar@innostark.com>
2015-01-20 20:28:08 +0500

    Service, Repository

--- EPMS.Web/ViewModels/Tasks/TaskCreateViewModel.cs
+++ EPMS.Web/ViewModels/Tasks/TaskCreateViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace EPMS.Web.ViewModels.Tasks
+﻿using EPMS.Web.Models;
+
+namespace EPMS.Web.ViewModels.Tasks
 {
     public class TaskCreateViewModel
     {
+        public ProjectTask ProjectTask { get; set; }
         public string PageTitle { get; set; }
         public string BtnText { get; set; }
         public string Header { get; set; }
