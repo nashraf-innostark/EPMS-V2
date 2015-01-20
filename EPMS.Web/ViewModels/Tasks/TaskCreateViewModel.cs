8087d4c6e9e60fcf9828200100c284473fdde060
istammar <ammar@innostark.com>
2015-01-20 16:48:26 +0500

    Task Create

---
+++ EPMS.Web/ViewModels/Tasks/TaskCreateViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EPMS.Web.ViewModels.Tasks
+{
+    public class TaskCreateViewModel
+    {
+        public string PageTitle { get; set; }
+        public string BtnText { get; set; }
+        public string Header { get; set; }
+    }
+}
--- EPMS.Web/ViewModels/Tasks/TaskListViewModel.cs
+++ EPMS.Web/ViewModels/Tasks/TaskListViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace EPMS.Web.ViewModels.Tasks
+﻿namespace EPMS.Web.ViewModels.Tasks
 {
     public class TaskListViewModel
     {
