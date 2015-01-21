b91684b4c64ed49317ea3e8da711518b0774a66e
hassanist <hassan@innostark.com>
2015-01-21 16:26:54 +0500

    Meeting Create

---
+++ EPMS.Web/Models/MeetingAttendee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPMS.Web.Models
+{
+    public class MeetingAttendee
+    {
+        public long AttendeeId { get; set; }
+        public long MeetingId { get; set; }
+        public long? EmployeeId { get; set; }
+        public string AttendeeName { get; set; }
+        public string AttendeeEmail { get; set; }
+        public bool? Status { get; set; }
+        public string RecCreatedBy { get; set; }
+        public DateTime? RecCreatedDt { get; set; }
+        public string RecLastUpdatedBy { get; set; }
+        public DateTime? RecLastUpdatedDt { get; set; }
+    }
+}
--- EPMS.Web/ViewModels/Meeting/MeetingViewModel.cs
+++ EPMS.Web/ViewModels/Meeting/MeetingViewModel.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using EPMS.Models.DomainModels;
 
 namespace EPMS.Web.ViewModels.Meeting
 {
     public class MeetingViewModel
     {
+        public MeetingViewModel()
+        {
+            Meeting = new Models.Meeting();
+            MeetingAttendee = new MeetingAttendee();
+            MeetingDocument = new MeetingDocument();
+        }
+        public Models.Meeting Meeting { get; set; }
+        public IEnumerable<Models.Meeting> MeetingList { get; set; }
+        public MeetingAttendee MeetingAttendee { get; set; }
+        public IEnumerable<MeetingAttendee> MeetingAttendees { get; set; }
+        public MeetingDocument MeetingDocument { get; set; }
+        public IEnumerable<MeetingDocument> MeetingDocuments { get; set; }
+        public IEnumerable<Models.Employee> Employees { get; set; }
+        public List<int> EmployeeId { get; set; }
+        public string AttendeeName { get; set; }
+        public string AttendeeEmail { get; set; }
+        public bool? Status { get; set; }
+
     }
 }
