63979bb7ace662f8bf5d24a674fad0540175866a
istammar <ammar@innostark.com>
2015-02-04 19:13:58 +0500

    Dashboard widget preferences

---
+++ EPMS.Repository/Repositories/DashboardWidgetPreferencesRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+using EPMS.Interfaces.Repository;
+using EPMS.Models.DomainModels;
+using EPMS.Repository.BaseRepository;
+using Microsoft.Practices.Unity;
+
+namespace EPMS.Repository.Repositories
+{
+    public class DashboardWidgetPreferencesRepository : BaseRepository<DashboardWidgetPreferences>, IDashboardWidgetPreferencesRepository
+    {
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public DashboardWidgetPreferencesRepository(IUnityContainer container)
+            : base(container)
+        {
+        }
+
+        /// <summary>
+        /// Primary database set
+        /// </summary>
+        protected override IDbSet<DashboardWidgetPreferences> DbSet
+        {
+            get { return db.Preferenceses; }
+        }
+
+        #endregion
+    }
+}
