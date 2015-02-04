7611ae307f359f18930d0a9d081c7ec4f095ca43
istammar <ammar@innostark.com>
2015-02-04 19:00:04 +0500

    Dashboard widgets sorting

---
+++ EPMS.Models/DomainModels/DashboardWidgetPreferences.cs
@@ -0,0 +1,10 @@
+﻿namespace EPMS.Models.DomainModels
+{
+    public class DashboardWidgetPreferences
+    {
+        public int WidgetPerferences { get; set; }
+        public string UserId { get; set; }
+        public string WidgetId { get; set; }
+        public string SortNumber { get; set; }
+    }
+}
