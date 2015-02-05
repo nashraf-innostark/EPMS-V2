ac1f3aaa02392f09d4af9282e926711b75b442cc
hassanist <hassan@innostark.com>
2015-02-05 18:27:15 +0500

    Services

---
+++ EPMS.Interfaces/Repository/IQuickLaunchItemsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using EPMS.Models.DomainModels;
+
+namespace EPMS.Interfaces.Repository
+{
+    public interface IQuickLaunchItemsRepository : IBaseRepository<QuickLaunchItems, long>
+    {
+        IEnumerable<QuickLaunchItems> FindItemsbyEmployeeId(long? employeeId);
+    }
+}
