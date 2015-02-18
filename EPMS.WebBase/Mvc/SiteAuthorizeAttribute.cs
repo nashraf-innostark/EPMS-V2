26837a9bbe3ffb228f463ada78a94cd57deca924
istammar <ammar@innostark.com>
2015-02-18 20:21:12 +0500

    Licensing

--- EPMS.WebBase/Mvc/SiteAuthorizeAttribute.cs
+++ EPMS.WebBase/Mvc/SiteAuthorizeAttribute.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Configuration;
+using System.DirectoryServices.ActiveDirectory;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -17,12 +23,59 @@ namespace EPMS.WebBase.Mvc
         /// </summary>
         private bool IsAuthorized()
         {
+            // check license
+            var licenseKeyEncrypted = ConfigurationManager.AppSettings["LicenseKey"].ToString(CultureInfo.InvariantCulture);
+            LicenseKey = EncryptDecrypt.StringCipher.Decrypt(licenseKeyEncrypted, "123");
+            var splitLicenseKey = LicenseKey.Split('|');
+            Domain = splitLicenseKey[0];
+            MacAddress = splitLicenseKey[1];
+            NoOfUsers = splitLicenseKey[2];
+            ExpiryDate = DateTime.ParseExact(splitLicenseKey[3], "dd/MM/yyyy", new CultureInfo("en"));
+            Modules = splitLicenseKey[4].Split(';');
+
+            // check MAC Address
+            string userMacAddress = GetMacAddress();
+            if (MacAddress != userMacAddress)
+            {
+                return false;
+            }
+
+            // check Domain
+            var dir = AppDomain.CurrentDomain.BaseDirectory;
+            if (dir != Domain)
+            {
+                return false;
+            }
+
+            //// check license expiry date
+            DateTime currDate = DateTime.Now;
+            if (currDate > ExpiryDate)
+            {
+                return false;
+            }
+
             object userPermissionSet = HttpContext.Current.Session["UserPermissionSet"];
+            string[] userPermissionsSet = new string[] {};
+            var permissionToSpecificController = false;
             if (userPermissionSet != null)
             {
-                string[] userPermissionsSet = (string[]) userPermissionSet;
-                return (userPermissionsSet.Contains(PermissionKey));
+                userPermissionsSet = (string[]) userPermissionSet;
+                permissionToSpecificController = (userPermissionsSet.Contains(PermissionKey));
             }
+            // check allowed modules
+            bool permissionToModule = false;
+            foreach (var module in Modules)
+            {
+                if (userPermissionsSet.Contains(module))
+                {
+                    permissionToModule = true;
+                }
+            }
+            if (permissionToModule && permissionToSpecificController)
+            {
+                return true;
+            }
+
             return false;
         }
         /// <summary>
@@ -53,7 +106,30 @@ namespace EPMS.WebBase.Mvc
                         new RouteValueDictionary(
                             new { area = "", controller = "UnauthorizedRequest", action = "Index" }));
             }
-        }        
+        }
+
+        #region Get MAC Address
+        public static string GetMacAddress()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            String sMacAddress = string.Empty;
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (sMacAddress == String.Empty)// only return MAC Address from first card  
+                {
+                    //IPInterfaceProperties properties = adapter.GetIPProperties(); Line is not required
+                    sMacAddress = adapter.GetPhysicalAddress().ToString();
+                }
+            } return sMacAddress;
+        }
+        #endregion
+
         public string PermissionKey { get; set; }        
+        public string LicenseKey { get; set; }        
+        public string Domain { get; set; }        
+        public string MacAddress { get; set; }        
+        public string[] Modules { get; set; }        
+        public string NoOfUsers { get; set; }        
+        public DateTime ExpiryDate { get; set; }        
     }
 }
