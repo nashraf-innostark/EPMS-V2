04a42b8b2bafd554042dc8b10c073953a7da0baa
istammar <ammar@innostark.com>
2015-03-11 13:49:44 +0500

    Remove Tags in Job Offered description.

--- EPMS.Web/ModelMappers/JobTitleMapper.cs
+++ EPMS.Web/ModelMappers/JobTitleMapper.cs
@@ -17,25 +17,27 @@ namespace EPMS.Web.ModelMappers
 
         public static Models.JobTitle CreateFrom(this DomainModels.JobTitle source)
         {
-            return new Models.JobTitle
-            {
-                JobTitleId = source.JobTitleId,
-                JobTitleNameE = source.JobTitleNameE,
-                JobTitleNameA = source.JobTitleNameA,
-                JobTitleDescE = source.JobTitleDescE,
-                JobTitleDescA = source.JobTitleDescA,
-                DepartmentId = source.DepartmentId,
-                BasicSalary = source.BasicSalary ?? 0,
-                DepartmentNameE = source.Department.DepartmentNameE,
-                DepartmentNameA = source.Department.DepartmentNameA,
-                RecCreatedBy = source.RecCreatedBy,
-                RecCreatedDt = source.RecCreatedDt,
-                RecLastUpdatedBy = source.RecLastUpdatedBy,
-                RecLastUpdatedDt = source.RecLastUpdatedDt,
-                EmployeesCount = source.Employees.Count
 
-            };
-
+            Models.JobTitle retVal = new ApiModels.JobTitle();
+            retVal.JobTitleId = source.JobTitleId;
+            retVal.JobTitleNameE = source.JobTitleNameE;
+            retVal.JobTitleNameA = source.JobTitleNameA;
+            var decspE = source.JobTitleDescE.Replace("\n", "");
+            decspE = decspE.Replace("\r", "");
+            retVal.JobTitleDescE = decspE;
+            var decspA = source.JobTitleDescA.Replace("\n", "");
+            decspA = decspA.Replace("\r", "");
+            retVal.JobTitleDescA = decspA;
+            retVal.DepartmentId = source.DepartmentId;
+            retVal.BasicSalary = source.BasicSalary ?? 0;
+            retVal.DepartmentNameE = source.Department.DepartmentNameE;
+            retVal.DepartmentNameA = source.Department.DepartmentNameA;
+            retVal.RecCreatedBy = source.RecCreatedBy;
+            retVal.RecCreatedDt = source.RecCreatedDt;
+            retVal.RecLastUpdatedBy = source.RecLastUpdatedBy;
+            retVal.RecLastUpdatedDt = source.RecLastUpdatedDt;
+            retVal.EmployeesCount = source.Employees.Count;
+            return retVal;
         }
         public static DomainModels.JobTitle CreateFrom(this Models.JobTitle source)
         {
