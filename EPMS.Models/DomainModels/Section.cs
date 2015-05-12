c75e3d81fc9f03c3d265f7f397cbc3852840525d
hassanist <hassan@innostark.com>
2015-05-12 12:22:46 +0500

    Models and EDMX

---
+++ EPMS.Models/DomainModels/Section.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPMS.Models.DomainModels
+{
+    public class Section
+    {
+        public long SectionId { get; set; }
+        public string SectionName { get; set; }
+        public long AisleId { get; set; }
+        public string RecCreatedBy { get; set; }
+        public DateTime RecCreatedDt { get; set; }
+        public string RecLastUpdatedBy { get; set; }
+        public DateTime RecLastUpdatedDt { get; set; }
+
+        public virtual Aisle Aisle { get; set; }
+        public virtual Shelf Shelf { get; set; }
+    }
+}
---
+++ EPMS.Models/DomainModels/Warehouse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPMS.Models.DomainModels
+{
+    public class Warehouse
+    {
+        public long WarehouseId { get; set; }
+        public string WarehouseNumber { get; set; }
+        public long? WarehouseManager { get; set; }
+        public string WarehouseLocation { get; set; }
+        public string RecCreatedBy { get; set; }
+        public DateTime RecCreatedDt { get; set; }
+        public string RecLastUpdatedBy { get; set; }
+        public DateTime RecLastUpdatedDt { get; set; }
+
+        public virtual ICollection<Aisle> Aisles { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
