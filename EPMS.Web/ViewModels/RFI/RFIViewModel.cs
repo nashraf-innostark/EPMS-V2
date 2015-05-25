5d883137fa4a739a62d0f9fde51b2b2a6068bde1
curiousnadir <nadir@innostark.com>
2015-05-25 11:38:03 +0500

    RFI

---
+++ EPMS.Web/ViewModels/RFI/RFIViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace EPMS.Web.ViewModels.RFI
+{
+    public class RFIViewModel
+    {
+        public IEnumerable<Models.Customer> Customers { get; set; }
+        public IEnumerable<Models.Order> Orders { get; set; }
+    }
+}
