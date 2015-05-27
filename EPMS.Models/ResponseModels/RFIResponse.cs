eaf489bb26049efe4dde8831fe612e03b62f5018
curiousnadir <nadir@innostark.com>
2015-05-27 15:22:00 +0500

    RFI Create Saving

---
+++ EPMS.Models/ResponseModels/RFIResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using EPMS.Models.DomainModels;
+
+namespace EPMS.Models.ResponseModels
+{
+    public class RFIResponse
+    {
+        public RFI Rfi { get; set; }
+        public IEnumerable<RFIItem> RfiItem { get; set; }
+        public IEnumerable<ItemVariationDropDownListItem> ItemVariationDropDownList { get; set; }
+    }
+}
---
+++ EPMS.Web/ModelMappers/Inventory/RFI/RFIMapper.cs
@@ -0,0 +1,43 @@
+﻿using EPMS.Web.Models;
+using EPMS.Web.ViewModels.RFI;
+namespace EPMS.Web.ModelMappers.Inventory.RFI
+{
+    public static class RFIMapper
+    {
+        public static EPMS.Models.DomainModels.RFI CreateRFIClientToServer(this RFIViewModel source)
+        {
+            var rfiItem = new EPMS.Models.DomainModels.RFI
+            {
+                RFIId = source.Rfi.RFIId,
+                OrderId = source.Rfi.OrderId,
+                UsageE = source.Rfi.UsageE,
+                UsageA = source.Rfi.UsageA,
+                RecCreatedBy = source.Rfi.RecCreatedBy,
+                RecCreatedDate = source.Rfi.RecCreatedDate,
+                RecUpdatedBy = source.Rfi.RecCreatedBy,
+                RecUpdatedDate = source.Rfi.RecUpdatedDate,
+
+
+            };
+            return null;
+        }
+
+        public static EPMS.Models.DomainModels.RFIItem CreateRFIItemClientToServer(this RFIItem source)
+        {
+            var rfiItem = new EPMS.Models.DomainModels.RFIItem
+            {
+                RFIItemId = source.RFIItemId,
+                RFIId = source.RFIId,
+                ItemVariationId = source.ItemVariationId,
+                
+                RecCreatedBy = source.RecCreatedBy,
+                RecCreatedDate = source.RecCreatedDate,
+                RecUpdatedBy = source.RecCreatedBy,
+                RecUpdatedDate = source.RecUpdatedDate,
+
+
+            };
+            return null;
+        }
+    }
+}
