348dea8588d76ed582d36549fd8b75425bbf47b4
istammar <ammar@innostark.com>
2015-05-27 15:17:11 +0500

    Inventory departments, section, sub sections

--- EPMS.Interfaces/Repository/IInventoryDepartmentRepository.cs
+++ EPMS.Interfaces/Repository/IInventoryDepartmentRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using EPMS.Models.DomainModels;
 
 namespace EPMS.Interfaces.Repository
@@ -10,5 +6,6 @@ namespace EPMS.Interfaces.Repository
     public interface IInventoryDepartmentRepository : IBaseRepository<InventoryDepartment, long>
     {
         bool DepartmentExists(InventoryDepartment department);
+        IEnumerable<InventoryDepartment> GetAllDepartments();
     }
 }
