a58520c4ba19f60a78715d6e259b06a964c67ada
istammar <ammar@innostark.com>
2015-05-28 15:01:16 +0500

    Inventory Warehouse LV

---
+++ EPMS.Web/Areas/Inventory/Controllers/InvontryWarehouseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPMS.Web.Controllers;
+
+namespace EPMS.Web.Areas.Inventory.Controllers
+{
+    public class InvontryWarehouseController : BaseController
+    {
+        // GET: Inventory/InvontryWarehouse
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
