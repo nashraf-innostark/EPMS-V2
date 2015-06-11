5fb75d9fc75f8b0f722678a334519062bb99f047
istammar <ammar@innostark.com>
2015-06-11 13:19:36 +0500

    ItemWarehouse web model

---
+++ EPMS.Web/Models/ItemWarehouse.cs
@@ -0,0 +1,9 @@
+﻿namespace EPMS.Web.Models
+{
+    public class ItemWarehouse
+    {
+        public long WarehousrId { get; set; }
+        public long ItemVariationId { get; set; }
+        public long Quantity { get; set; }
+    }
+}
