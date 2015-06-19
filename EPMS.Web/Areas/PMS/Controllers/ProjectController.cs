dfa789d30235ee5eddd2c50346174c33dd521802
istammar <ammar@innostark.com>
2015-06-19 15:37:25 +0500

    Task progress in Project detail

--- EPMS.Web/Areas/PMS/Controllers/ProjectController.cs
+++ EPMS.Web/Areas/PMS/Controllers/ProjectController.cs
@@ -285,7 +285,7 @@ namespace EPMS.Web.Areas.PMS.Controllers
                     var projectTasks = projectTaskService.GetTasksByProjectId((long)id);
                     if (projectTasks != null)
                     {
-                        projectViewModel.ProjectTasks = projectTasks.Select(x => x.CreateFromServerToClientCreate());
+                        projectViewModel.ProjectTasks = projectTasks.Select(x => x.CreateFromServerToClientCreateForProjectDetails());
                         foreach (var projectTask in projectViewModel.ProjectTasks)
                         {
                             projectViewModel.Project.TotalTasksCost += projectTask.TotalCost;
