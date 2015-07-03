ca7594c906823357d89b4e557fc9a3b47b8f9c75
istammar <ammar@innostark.com>
2015-07-03 16:23:05 +0500

    Barcode printer

---
+++ EPMS.Web/Models/Barcode.cs
@@ -0,0 +1,9 @@
+﻿namespace EPMS.Web.Models
+{
+    public class Barcode
+    {
+        public string ItemDetails { get; set; }
+        public string BarcodeValue { get; set; }
+        public string Quantity { get; set; }
+    }
+}
