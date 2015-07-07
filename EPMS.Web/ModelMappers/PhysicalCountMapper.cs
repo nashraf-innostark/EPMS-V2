1bf992b41887bcacae45d5e3d8f3d3bc018dfa5e
curiousnadir <nadir@innostark.com>
2015-07-07 16:32:59 +0500

    Physical Count Model

---
+++ EPMS.Web/ModelMappers/PhysicalCountMapper.cs
@@ -0,0 +1,24 @@
+﻿using EPMS.Models.DomainModels;
+using EPMS.Web.Models;
+
+namespace EPMS.Web.ModelMappers
+{
+    public static class PhysicalCountMapper
+    {
+        public static PhysicalCountListModel CreateListFromServerToClient(this PhysicalCount source)
+        {
+            return new PhysicalCountListModel
+            {
+                PCId = source.PCId
+            };
+        }
+        public static PhysicalCountModel CreateFromServerToClient(this PhysicalCount source)
+        {
+            return new PhysicalCountModel
+            {
+                PCId = source.PCId,
+
+            };
+        }
+    }
+}
---
+++ EPMS.Web/Models/PhysicalCountModel.cs
@@ -0,0 +1,19 @@
+﻿namespace EPMS.Web.Models
+{
+    public class PhysicalCountModel
+    {
+        public long PCId { get; set; }
+        public long ItemVariationId { get; set; }
+        public long WarehouseId { get; set; }
+        public long NoOfPackagesInWarehouse { get; set; }
+        public long NoOfItemInWarehouse { get; set; }
+        public string ItemDetails { get; set; }
+        public long ItemsInPackage { get; set; }
+        public long TotalItemsInPackages { get; set; }
+        public long TotalItemsCount { get; set; }
+        public string RecCreatedBy { get; set; }
+        public System.DateTime RecCreatedDate { get; set; }
+        public string RecLastUpdatedBy { get; set; }
+        public System.DateTime RecLastUpdatedDate { get; set; }
+    }
+}
