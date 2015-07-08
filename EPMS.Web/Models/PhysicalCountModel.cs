6b9b6b71ee55c40e6efd0bc41a015c07f2412320
curiousnadir <nadir@innostark.com>
2015-07-08 11:11:30 +0500

    PhysicalCountItem

--- EPMS.Web/Models/PhysicalCountModel.cs
+++ EPMS.Web/Models/PhysicalCountModel.cs
@@ -3,15 +3,7 @@
     public class PhysicalCountModel
     {
         public long PCId { get; set; }
-        public long ItemVariationId { get; set; }
-        public long WarehouseId { get; set; }
-        public long NoOfPackagesInWarehouse { get; set; }
-        public long NoOfItemInWarehouse { get; set; }
-        public string ItemDetailsEn { get; set; }
-        public string ItemDetailsAr { get; set; }
-        public long ItemsInPackage { get; set; }
-        public long TotalItemsInPackages { get; set; }
-        public long TotalItemsCount { get; set; }
+       
         public string RecCreatedByName { get; set; }
         public string RecCreatedBy { get; set; }
         public System.DateTime RecCreatedDate { get; set; }
