6e43060a21619dfd59f28b8b76932ef7b1976eaf
hassanist <hassan@innostark.com>
2015-07-30 18:19:49 +0500

    ContactUs Server Side

---
+++ EPMS.Implementation/Services/ContactUsService.cs
@@ -0,0 +1,39 @@
+﻿using EPMS.Interfaces.IServices;
+using EPMS.Interfaces.Repository;
+using EPMS.Models.DomainModels;
+
+namespace EPMS.Implementation.Services
+{
+    public class ContactUsService : IContactUsService
+    {
+        private readonly IContactUsRepository contactUsRepository;
+
+        public ContactUsService(IContactUsRepository contactUsRepository)
+        {
+            this.contactUsRepository = contactUsRepository;
+        }
+
+        #region Public
+
+        public bool AddDetail(ContactUs contactUs)
+        {
+            contactUsRepository.Add(contactUs);
+            contactUsRepository.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateDetail(ContactUs contactUs)
+        {
+            contactUsRepository.Update(contactUs);
+            contactUsRepository.SaveChanges();
+            return true;
+        }
+
+        public ContactUs GetDetail()
+        {
+            return contactUsRepository.GetContactUs();
+        }
+
+        #endregion
+    }
+}
---
+++ EPMS.Interfaces/IServices/IContactUsService.cs
@@ -0,0 +1,21 @@
+﻿using EPMS.Models.DomainModels;
+
+namespace EPMS.Interfaces.IServices
+{
+    public interface  IContactUsService
+    {
+        /// <summary>
+        /// Add ContactUs Details
+        /// </summary>
+        bool AddDetail(ContactUs contactUs);
+        /// <summary>
+        /// Update ContactUs Details
+        /// </summary>
+        bool UpdateDetail(ContactUs contactUs);
+
+        /// <summary>
+        /// Get ContactUs Details
+        /// </summary>
+        ContactUs GetDetail();
+    }
+}
