af6e92809c4ad7acf62df6d457a3cb1b93670973
hassanist <hassan@innostark.com>
2015-08-05 11:04:23 +0500

    Bug Fixes

--- EPMS.Implementation/Services/ItemVariationService.cs
+++ EPMS.Implementation/Services/ItemVariationService.cs
@@ -82,6 +82,7 @@ namespace EPMS.Implementation.Services
             var item = variationRepository.FindVariationByBarcode(barcode);
             if (item != null)
             {
+                
                 return new PCFromBarcodeResponse
                 {
                     ItemBarcode = barcode,
@@ -91,7 +92,8 @@ namespace EPMS.Implementation.Services
                     SKUDescriptionEn = item.SKUDescriptionEn,
                     SKUDescriptionAr = item.SKUDescriptionAr,
                     ItemsInPackage = item.InventoryItem.QuantityInPackage ?? 0,
-                    TotalItemsCountInWarehouse = item.ItemWarehouses.Sum(x => x.Quantity)
+                    //TotalItemsCountInWarehouse = item.ItemWarehouses.Sum(x => x.Quantity)
+                    TotalItemsCountInWarehouse = item.ItemWarehouses.Sum(x => x.Quantity) - item.ItemReleaseQuantities.Sum(x=>x.Quantity),
                 };
             }
             return null;
