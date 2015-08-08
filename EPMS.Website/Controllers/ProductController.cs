d83bd82792068c36fcf21291a524d1573b2e3a9b
curiousnadir <nadir@innostark.com>
2015-08-08 16:58:08 +0500

    Products page

---
+++ EPMS.Website/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace EPMS.Website.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(long id)
+        {
+            return View();
+        }
+    }
+}
