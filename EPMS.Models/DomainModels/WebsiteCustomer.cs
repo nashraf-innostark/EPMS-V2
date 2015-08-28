9acd1e5a6b76802a74ff37ed2993680d6ae3ceb2
ISTAmmar <ammar@innostark.com>
2015-08-28 17:32:51 +0500

    Website Customer DB table, models, EDMX

---
+++ EPMS.Models/DomainModels/WebsiteCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EPMS.Models.DomainModels
+{
+    public class WebsiteCustomer
+    {
+        public long CustomerId { get; set; }
+        public string CustomerNameEn { get; set; }
+        public string CustomerNameAr { get; set; }
+        public string RecCreatedBy { get; set; }
+        public DateTime RecCreatedDate { get; set; }
+        public string RecUpdatedBy { get; set; }
+        public DateTime RecUpdatedDate { get; set; }
+
+        public virtual ICollection<AspNetUser> AspNetUsers { get; set; }
+    }
+}
---
+++ EPMS.WebModels/WebsiteModels/WebsiteCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EPMS.WebModels.WebsiteModels
+{
+    public class WebsiteCustomer
+    {
+        public long CustomerId { get; set; }
+        public string CustomerNameEn { get; set; }
+        public string CustomerNameAr { get; set; }
+        public string RecCreatedBy { get; set; }
+        public DateTime RecCreatedDate { get; set; }
+        public string RecUpdatedBy { get; set; }
+        public DateTime RecUpdatedDate { get; set; }
+    }
+}
