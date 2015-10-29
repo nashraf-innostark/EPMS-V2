54d3d6aedfbb3e184a0f179db62024c0b480ad53
istammar <ammar@innostark.com>
2015-10-29 19:03:25 +0500

    move changes of inventory to website model separation

---
+++ EPMS.Web/Areas/Api/Controllers/TreeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using EPMS.Interfaces.IServices;
+using EPMS.Web.Controllers;
+using EPMS.Web.ModelMappers;
+using EPMS.Web.Models.Common;
+
+namespace EPMS.Web.Areas.Api.Controllers
+{
+    public class TreeController : BaseController
+    {
+        #region Private
+
+        private readonly IInventoryDepartmentService departmentService;
+
+        #endregion
+
+        #region Constructor
+        public TreeController(IInventoryDepartmentService departmentService)
+        {
+            this.departmentService = departmentService;
+        }
+
+        #endregion
+
+        [HttpGet]
+        public JsonResult GetTreeData(long? id, string direction)
+        {
+            var departments = departmentService.GetAll();
+            IList<JsTreeJson> details = Utility.InventoryDepartmentTree(departments, direction);
+            var serializer = new JavaScriptSerializer();
+            var serializedResult = serializer.Serialize(details);
+            return Json(serializedResult, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
