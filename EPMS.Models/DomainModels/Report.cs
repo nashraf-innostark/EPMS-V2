34ecc689b95589fef8e8349fd424d89ccdfbe1a1
curiousnadir <nadir@innostark.com>
2015-11-03 17:19:42 +0500

    Reports Table

---
+++ EPMS.Models/DomainModels/Report.cs
@@ -0,0 +1,15 @@
+﻿namespace EPMS.Models.DomainModels
+{
+    public class Report
+    {
+        public long ReportId { get; set; }
+        public int ReportCategoryId { get; set; }
+        public long ReportCategoryItemId { get; set; }
+        public System.DateTime ReportFromDate { get; set; }
+        public System.DateTime ReportToDate { get; set; }
+        public long ReportCreatedBy { get; set; }
+        public System.DateTime ReportCreatedDate { get; set; }
+
+        public virtual Employee Employee { get; set; }
+    }
+}
