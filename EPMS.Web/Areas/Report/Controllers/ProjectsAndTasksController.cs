d6ec4436b30ff384e8dd926c317e00da8439094a
curiousnadir <nadir@innostark.com>
2015-11-05 15:32:33 +0500

    Project Reports

--- EPMS.Web/Areas/Report/Controllers/ProjectsAndTasksController.cs
+++ EPMS.Web/Areas/Report/Controllers/ProjectsAndTasksController.cs
@@ -1,14 +1,46 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPMS.Implementation.Services;
+using EPMS.Models.RequestModels;
+using EPMS.Models.ResponseModels;
+using EPMS.Web.Controllers;
+using EPMS.WebModels.ModelMappers;
+using EPMS.WebModels.ViewModels.Employee;
+using EPMS.WebModels.ViewModels.Reports;
+using EPMS.WebModels.WebsiteModels;
+using Microsoft.AspNet.Identity;
 
 namespace EPMS.Web.Areas.Report.Controllers
 {
-    public class ProjectsAndTasksController : Controller
+    public class ProjectsAndTasksController : BaseController
     {
-        // GET: Report/ProjectsAndTasks
+        public ProjectsAndTasksController()
+        {
+            
+        }
         public ActionResult Index()
         {
+            var projectsReports = new ProjectsReportsListViewModel();
 
-            return View();
+            return View(projectsReports);
+        }
+        [HttpPost]
+        public ActionResult Index(ProjectReportSearchRequest searchRequest)
+        {
+            //searchRequest.SearchString = Request["search"];
+            //var projectsAndTasksResponse = EmployeeService.GetAllEmployees(searchRequest);
+            //IEnumerable<Employee> employeeList =
+            //    employees.Employeess.Select(x => x.CreateFromServerToClientWithImage()).ToList();
+            //EmployeeViewModel employeeViewModel = new EmployeeViewModel
+            //{
+            //    aaData = employeeList,
+            //    iTotalRecords = Convert.ToInt32(employees.TotalRecords),
+            //    iTotalDisplayRecords = Convert.ToInt32(employees.TotalDisplayRecords),
+            //    sEcho = searchRequest.sEcho
+            //};
+            //return Json(employeeViewModel, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ EPMS.WebModels/ViewModels/Reports/ProjectsReportsListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using EPMS.WebModels.WebsiteModels;
+
+namespace EPMS.WebModels.ViewModels.Reports
+{
+    public class ProjectsReportsListViewModel
+    {
+        public ProjectsReportsListViewModel()
+        {
+            Reports=new List<Report>();
+        }
+        public List<Report> Reports { get; set; }
+        /// <summary>
+        /// Total Records in DB
+        /// </summary>
+        public int iTotalRecords;
+
+        /// <summary>
+        /// Total Records Filtered
+        /// </summary>
+        public int iTotalDisplayRecords;
+
+        public string sEcho;
+    }
+}
