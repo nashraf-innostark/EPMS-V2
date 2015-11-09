f9be17e27c1a425e379552114402a82ab6ad3ee4
curiousnadir <nadir@innostark.com>
2015-11-09 20:30:14 +0500

    PDF (Pending)

--- EPMS.Models/ResponseModels/ReportsResponseModels/ProjectReportDetailsResponse.cs
+++ EPMS.Models/ResponseModels/ReportsResponseModels/ProjectReportDetailsResponse.cs
@@ -4,6 +4,7 @@ namespace EPMS.Models.ResponseModels.ReportsResponseModels
 {
     public class ProjectReportDetailsResponse
     {
+        public long ReportId { get; set; }
         public IEnumerable<DomainModels.Project> Projects { get; set; }
         public IEnumerable<DomainModels.ProjectTask> ProjectTasks { get; set; }
     }
