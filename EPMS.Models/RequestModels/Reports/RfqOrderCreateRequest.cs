5d7da9e122a36734d562eb9fc5dcd19623f1824e
istammar <ammar@innostark.com>
2015-11-19 12:02:28 +0500

    Rfq/Order Create request model

---
+++ EPMS.Models/RequestModels/Reports/RfqOrderCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EPMS.Models.RequestModels.Reports
+{
+    public class RfqOrderCreateRequest
+    {
+        public int ReportCategoryId { get; set; }
+        public DateTime ReportFromDate { get; set; }
+        public DateTime ReportToDate { get; set; }
+        public string ReportCreatedBy { get; set; }
+        public DateTime ReportCreatedDate { get; set; }
+        public long? CustomerId { get; set; }
+    }
+}
