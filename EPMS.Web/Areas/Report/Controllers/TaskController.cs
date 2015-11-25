00fe2f59f6fcb0ef9cc41777687028e72deb9135
curiousnadir <nadir@innostark.com>
2015-11-25 20:45:27 +0500

    tasks report fixes

--- EPMS.Web/Areas/Report/Controllers/TaskController.cs
+++ EPMS.Web/Areas/Report/Controllers/TaskController.cs
@@ -66,7 +66,7 @@ namespace EPMS.Web.Areas.Report.Controllers
                 SubTasks = response.SubTasks.Select(x => x.CreateFromServerToClientLv()).ToList()
             };
             SetGraphData(detailViewModel);
-            if ((viewModel.ProjectId == 0 && viewModel.TaskId == 0))
+            if ((viewModel.ProjectId == 0 && viewModel.TaskId == 0 && viewModel.ReportId == 0))
             {
                 return RedirectToAction("All", new {ReportId = detailViewModel.ReportId});
             }
