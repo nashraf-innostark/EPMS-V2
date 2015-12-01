74dbc25c8adbdd566f5efdde77e737016e8b2143
Hassan <hassan@innostark.com>
2015-12-01 15:33:34 +0500

    EDMX Update

---
+++ EPMS.Models/DomainModels/ReportQuotationInvoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPMS.Models.DomainModels
+{
+    public class ReportQuotationInvoice
+    {
+        public long ReportQuotationInvoiceId { get; set; }
+        public long ReportId { get; set; }
+        public long CustomerId { get; set; }
+        public string CustomerNameA { get; set; }
+        public string CustomerNameE { get; set; }
+        public int NoOfQuotations { get; set; }
+        public int NoOfInvoices { get; set; }
+
+        public virtual ICollection<ReportQuotationInvoiceItem> ReportQuotationInvoiceItems { get; set; }
+    }
+}
