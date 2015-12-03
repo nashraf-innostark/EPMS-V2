2ab87d9e05fb47254d08cc561ba8a8fd66c6cea9
istammar <ammar@innostark.com>
2015-12-03 14:14:58 +0500

    Bugs fixed

--- EPMS.WebModels/ViewModels/RFQ/RFQCreateViewModel.cs
+++ EPMS.WebModels/ViewModels/RFQ/RFQCreateViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace EPMS.WebModels.ViewModels.RFQ
+﻿using System.Collections.Generic;
+using EPMS.WebModels.WebsiteModels;
+
+namespace EPMS.WebModels.ViewModels.RFQ
 {
     public class RFQCreateViewModel
     {
         public RFQCreateViewModel()
         {
-            Rfq = new WebsiteModels.RFQ();
+            Rfq = new WebsiteModels.RFQ{ RFQItems = new List<RFQItem>()};
         }
         public WebsiteModels.RFQ Rfq { get; set; }
         public bool FromClient { get; set; }
