dfce134adf9f65f7163f8a7e2c7d4111bbfd8d82
istammar <ammar@innostark.com>
2016-02-29 16:35:42 +0500

    report sub module check in Rights Management

--- EPMS.Models/MenuModels/Rights.cs
+++ EPMS.Models/MenuModels/Rights.cs
@@ -6,6 +6,7 @@
         public string MenuTitle { get; set; }
         public string MenuTitleA { get; set; }
         public bool IsSelected { get; set; }
+        public string PermissionKey { get; set; }
 
         public bool IsParent { get; set; }
         public int? ParentId { get; set; }
